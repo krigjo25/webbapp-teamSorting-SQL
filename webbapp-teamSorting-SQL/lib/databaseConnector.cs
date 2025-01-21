eddb49dbc49771cf4df663607813b1c5b3f9c47c
Kristoffer <krigjo25@gmail.com>
2025-01-21 13:48:55 +0100

    ic

--- webbapp-teamSorting-SQL/lib/databaseConnector.cs
+++ webbapp-teamSorting-SQL/lib/databaseConnector.cs
@@ -11,7 +11,7 @@ public class SQLConnector
         //  Open the connection
         Conn.Open();
         
-        //  Create the query
+        //  Initialize the query
         string query = "CREATE DATABASE IF NOT EXISTS teamSorting";
         
         //  Execute the query
@@ -50,6 +50,76 @@ public class SQLConnector
         
         // Close the connection
         Conn.Close();
+    }
 
+    public void InsertData(string table, List<string> column, List<string> Data)
+    {
+        //  Open the connection
+        Conn.Open();
+        
+        //  Initialize the querys
+        string columns = "(";
+        string data = "(";
+
+        //  Add columns into the query
+        foreach (var elements in column)
+        {
+            columns += elements;
+            
+            if (elements != column.Last())
+            {
+                columns += ", ";
+            }
+            else
+            {
+                columns += ")";
+            }
+        }
+
+        //  Add data into the query
+        foreach (var element in Data)
+        {
+            //  Initialize the data
+            data += element;
+
+            if (element != Data.Last())
+            {
+                data += ", ";
+            }
+            else
+            {
+                data += ");";
+            }
+        }
+        
+        //  Initializing the query
+        string query = $"INSERT INTO {table} {columns} VALUES {data}";
+        
+        //  Execute the query
+        var cmd = new SqlCommand(query, Conn);
+        cmd.ExecuteNonQuery();
+        
+        //  Close the connection
+        Conn.Close();
+    }
+
+
+    public void CreateView()
+    {
+        
+    }
+}
+
+public class FileParser
+{
+
+}
+
+public class APIConnector
+{
+
+    public void GetTeams()
+    {
+        
     }
 }
